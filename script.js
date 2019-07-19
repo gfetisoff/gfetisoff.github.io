988de2a48bca0f900aa2e9bea2d77482eae690de
gfetisoff <53100711+gfetisoff@users.noreply.github.com>
2019-07-19 15:45:40 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function addnum(){
+        var x = parseFloat(document.getElementById('numberin1').value);
+        var y = parseFloat(document.getElementById('numberin2').value);
+        window.alert("The answer for " + x + "+" + y +" is: " + (x+y));
+}
